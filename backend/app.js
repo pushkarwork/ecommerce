63fabb28c87ba25aa3850df41155431f5458cab4
Pushkar Verma <pushkar753@gmail.com>
2023-08-31 12:26:01 +0530

    Creation of routes and controller files

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json())
+
+
+const product = require("./routes/productroutes");
+app.use("/api/v1", product)
+
 
 module.exports = app;
