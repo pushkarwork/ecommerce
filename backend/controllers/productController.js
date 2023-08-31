c37832da718e330eedc5579bde462693060c5330
Pushkar Verma <pushkar753@gmail.com>
2023-08-31 19:10:17 +0530

    Added all CRUD op. of Products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,7 @@
 const productModel = require("../models/productModel")
 
 
-
+// GET ALL PRODUCTS
 exports.getAllProducts = async (req, res) => {
     const products = await productModel.find()
     res.status(200).json({
@@ -11,9 +11,64 @@ exports.getAllProducts = async (req, res) => {
 
 }
 
+// CREATE NEW PRODUCT
 exports.createProduct = async (req, res, next) => {
     const product = await productModel.create(req.body)
     res.status(201).json({
+        success: true,
+        product
+    })
+}
+
+// UPDATE PRODUCT
+exports.updateProduct = async (req, res, next) => {
+    let product = await productModel.findById(req.params.id)
+
+    if (!product) {
+        res.status(500).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+    product = await productModel.findByIdAndUpdate(req.params.id, req.body, {
+        new: true, runValidators: true, useFindAndModify: false
+    })
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
+// DELETE PRODUCT
+exports.deleteProduct = async (req, res, next) => {
+    let product = await productModel.findById(req.params.id)
+
+    if (!product) {
+        res.status(500).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+    product = await productModel.findByIdAndRemove(req.params.id)
+
+
+    res.status(200).json({
+        success: true,
+        message: "Product deleted Successfully"
+    })
+}
+
+// GET SINGLE PRODUCT DETAILS
+exports.getAProduct = async (req, res, next) => {
+    let product = await productModel.findById(req.params.id)
+
+    if (!product) {
+        res.status(500).json({
+            success: false,
+            message: "Product not found"
+        })
+    }
+    res.status(200).json({
         success: true,
         product
     })
--- backend/routes/productroutes.js
+++ backend/routes/productroutes.js
@@ -3,9 +3,10 @@ const express = require("express");
 const router = express.Router();
 
 
-const { getAllProducts, createProduct } = require("../controllers/productController")
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getAProduct } = require("../controllers/productController")
 router.route('/products').get(getAllProducts);
 router.route("/products").post(createProduct);
+router.route("/products/:id").put(updateProduct).delete(deleteProduct).get(getAProduct)
 
 
 module.exports = router;
