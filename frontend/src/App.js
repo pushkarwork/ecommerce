f6248ef753b20060d10426d6c46358d1da66f95c
Pushkar Verma <pushkar753@gmail.com>
2023-09-06 12:51:35 +0530

    initial frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/layout/Header";
+import { BrowserRouter as Router } from "react-router-dom"
+
+function App() {
+  return (<>
+    <Router>
+      <Header />  
+    </Router>
+
+  </>);
+}
+
+export default App;
